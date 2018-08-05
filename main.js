12c0c96c1cea57bf63fb99c3d50ad197978ba223
David Rodriguez <33043921+Drod217@users.noreply.github.com>
2018-08-05 17:52:26 -0500

    fixed mobile responsiveness

--- main.js
+++ main.js
@@ -1,6 +1,43 @@
+window.onresize = function() {
+  this.location.reload();
+};
+
 var navFunds = document.querySelector(".nav-funds");
 var balance = 0.0;
 navFunds.addEventListener("click", function(e) {
   balance += 1;
   document.querySelector(".money").textContent = balance.toFixed(2);
+});
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(e) {
+  if (document.querySelector(".left-box").style.display === "none") {
+    document.querySelector(".left-box").style =
+      "display: block; width: 100%; height: 12vh;";
+  } else {
+    document.querySelector(".left-box").style.display = "none";
+  }
+});
+
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(e) {
+  if (document.querySelector(".funds").style.display === "none") {
+    document.querySelector(".funds").style =
+      "display: block; width: 100%; height: 12vh;";
+  } else {
+    document.querySelector(".funds").style.display = "none";
+  }
+});
+var hamburger = document.querySelector(".hamburger");
+hamburger.addEventListener("click", function(e) {
+  if (document.querySelector(".nav-funds").style.display === "none") {
+    document.querySelector(".nav-funds").style =
+      "display: block; width: 100%; height: 12vh;";
+  } else {
+    document.querySelector(".nav-funds").style.display = "none";
+  }
 });
